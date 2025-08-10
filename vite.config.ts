c902be0e7ff05ac7d1ffe3e6c659de74ebf50d1f
RJ <154748733+Titania2@users.noreply.github.com>
2025-08-10 19:52:58 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: 'ICX-AIESEC', // 👈 replace with your repo name
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
