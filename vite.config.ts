de2cbd81a978c363957c6013d2a61d40fbe6141b
RJ <154748733+Titania2@users.noreply.github.com>
2025-08-13 18:14:01 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/ICX-AIESEC/', // 👈 replace with your repo name
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        admin: 'admin/index.html',
+      },
+    },
   },
 });
